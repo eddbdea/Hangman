545cf4c47130c02f7a41d0860c83eed0bd41fea0
Eduard Badea <eduardbadea@outlook.com>
2025-01-23 16:28:35 +0200

    Function fix (script.js)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const word = ['a', 's', 'c', 'u', 'n', 's'];
 let lives = 7;
 let correctAnswer = -1;
-const frq = new Array(6).fill(0);
+const frq = new Array(word.length).fill(0);
 
 function submitSearchLetter() {
     const letter = document.getElementById('letter').value
@@ -9,10 +9,18 @@ function submitSearchLetter() {
     let index = positionChecked(letter);
     ++frq[index];
     console.log(index);
-    if(index >= 0 && index <= 5) {
+    if(index >= 0 && index <= 5 && frq[index] === 1) {
         let p =  document.getElementById('let' + index);
         p.removeAttribute('hidden');
         ++correctAnswer;
+        if (lives > 0 && correctAnswer === word.length - 1) {
+            document.getElementById('count').innerHTML = 'You won!';
+            setTimeout(function() {
+                location.reload();
+            }, 3000);
+            lives = 7;
+            correctAnswer = - 1;
+        }
     } else {
         --lives;
         if (lives === 0 && correctAnswer < word.length) {
@@ -22,15 +30,7 @@ function submitSearchLetter() {
             }, 3000);
             lives = 7;
             correctAnswer = - 1;
-        } else if (lives > 0 && correctAnswer === word.length) {
-            document.getElementById('count').innerHTML = 'You won!';
-            setTimeout(function() {
-                location.reload();
-            }, 3000);
-            lives = 7;
-            correctAnswer = - 1;
-        }
-        else {
+        } else {
             document.getElementById('count').innerHTML = 'Lives count: ' + lives;
         }
     }
@@ -39,7 +39,7 @@ function submitSearchLetter() {
 function positionChecked(letter) {
     let index = word.indexOf(letter);
     if (frq[index] === 1) {
-        index = word.indexOf(letter, index + 2);
+        index = word.indexOf(letter, index + 1);
         return index;
     }
     return index;
