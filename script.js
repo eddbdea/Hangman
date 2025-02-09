e543741534c4c8112aad739acecbb08c3c2167e3
Eduard Badea <eduardbadea@outlook.com>
2025-02-09 18:12:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const words = ['bag', 'apple', 'phone', 'night', 'dog', 'summer'];
 const MAX_LIVES = 7;
 const PAGE_RELOAD_SPEED = 3000;
-let lives = MAX_LIVES;
+const lives = MAX_LIVES;
 let correctAnswer = -1;
 let choosenWord = [];
 const frq = new Array();
 
 function randomNumber() {
     const maxLength = words.length;
-    let number = Math.floor(Math.random() * maxLength + 1);
+    let number = Math.floor(Math.random() * maxLength);
     return number;
 }
 
